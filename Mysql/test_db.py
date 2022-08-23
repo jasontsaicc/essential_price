301626e1aad4e4d94d2c7fa0390d73d1a637c4a6
irissu0415 <110445945+irissu0415@users.noreply.github.com>
2022-08-23 15:58:37 +0800

    Test for MongoDB and MySQL connection

---
+++ Mysql/test_db.py
@@ -0,0 +1,50 @@
+import pymongo
+from pymongo import MongoClient
+import mysql.connector
+from mysql.connector import errorcode
+
+# mongodb cloud connection
+client = MongoClient('mongodb+srv://admin:tgi102aaa@cluster0.19rsmeq.mongodb.net/?retryWrites=true&w=majority')
+
+db = client.All_Market
+collections = db.productions
+
+# RDS connection
+config = {
+  'host':'projectdb.ckq7h3eivlb4.ap-northeast-1.rds.amazonaws.com',
+  'user':'admin',
+  'password':'tgi102aaa',
+  'database':'essential'
+}
+
+conn = mysql.connector.connect(**config)
+print("Connection established")
+
+# # create table
+cursor = conn.cursor()
+cursor.execute(
+  """CREATE TABLE product (
+    id serial PRIMARY KEY, 
+    Category VARCHAR(50), 
+    Date DATE, 
+    Market VARCHAR(50), 
+    Price INTEGER, 
+    Product_name VARCHAR(50),
+    Url VARCHAR(150));
+    """)
+
+# find from mongodb(不顯示id)
+result = collections.find({}, {'_id':0})
+result_list = []
+for i in result:
+    result_list.append(i)
+    # print(type(i)) # 回傳結果是dic類型
+    # print(i)
+    for k, v in i.items():
+      # print(k, v)
+      # print(type(k), type(v))
+      cursor.execute("INSERT INTO product (Category, Date, Market, Price, Product_name, Url) VALUES (%s, %s, %s, %s, %s, %s);", (v, v+1, v+2, int(v+3), v+4, v+5))
+      print("Inserted",cursor.rowcount,"row(s) of data.")
+
+print(result_list)
+print()
