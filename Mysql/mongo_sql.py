0d75050d7d2b3a71d579276737767998a2ff8308
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-25 15:35:44 +0800

    update_mogodb_to_sql

--- Mysql/mongo_sql.py
+++ Mysql/mongo_sql.py
@@ -1,49 +1,54 @@
 import pymongo
 from pymongo import MongoClient
-from sql_model import Mart, Product, Category, Price, BASE
+from sql_model import Mart, Product, Category, Price, BASE, Session, engine
 import sqlalchemy as sa
 import pandas as pd
+import json
+import datetime
 
 # mongodb cloud connection
-client = MongoClient('mongodb+srv://admin:00065638@cluster0.fiqpaqd.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://admin:tgi102aaa@cluster0.19rsmeq.mongodb.net/?retryWrites=true&w=majority')
 
-mongo_db = client.test
-collections = mongo_db.pxmart_product
-result = collections.find({}, {'_id': 0})
+session = Session()
+
+date = datetime.datetime.now()
+update_date = date.date()
+
+mongo_db = client.All_Market
+collections = mongo_db.PxMart
+result = collections.find({'date': "2022-08-24"}, {'_id': 0}).limit(500)
+
+# result = collections.find({'date': f'{update_date}'}, {'_id': 0}).limit(15)
 result_list = []
 
 for i in result:
     result_list.append(i)
-print(result_list)
-for k, v in result_list[0].items():
-    print(k, v)
 
 
+Product_obj_list = []
+price_obj_list = []
 
-# data1 = Product(product_name=result_list[4], category_id=8, mart_id="P")
-# data2 = Price(date=, price=result_list[4]['price'], product_id=data1.id)
+for i in result_list:
+    category = i['category']
+    date = i['date']
+    market = i['market']
+    price = i['price']
+    product_name = i['product_name']
+    product_url = i['product_url']
+    product_pic_url = i['photos']
+
+    Product_obj = Product(product_name=product_name, product_url=product_url, product_pic_url=product_pic_url,
+                          category_id=category, mart_id=market)
+    Product_obj_list.append(Product_obj)
+    session.add_all(Product_obj_list)
+    session.flush()
+    price_obj = Price(date=date, price=price, product_id=Product_obj.id)
+    price_obj_list.append(price_obj)
 
 
+# select ID from
 
-
-
-
-# # 使用sa.create_engine設定數據庫的連接信息
-# engine = sa.create_engine('mysql+pymysql://root:00065638@localhost:3306/demo1', echo=True)
-# # 定義session 可以去連接數據庫 用來插入數據, 查詢數據
-# Session = sa.orm.sessionmaker(bind=engine)
-# # 通過調用Base裡面的metadata.create_all()方法把engine傳入去建立數據庫
-# # 會把BASE CLASS的子類(現在是user) 建立到數據庫裡面
-# BASE.metadata.create_all(engine)
-#
-# data1 = Product(product_name=result_list[4], category_id=8, mart_id="P")
-#
-#
-# # 建立一個session
-# session = Session()
-# # 有了session後 就可以調用session.add()去插入數據
-# # 插入多筆數據
-# session.add(data1)
-# #
-# # # 提交數據
-# session.commit()
+# 插入多筆數據
+session.add_all(price_obj_list)
+# # # # 提交數據
+session.commit()
