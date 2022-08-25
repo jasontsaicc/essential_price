7d39b034e2ad6741925c5525eee0783ccc9b6158
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-25 12:36:31 +0800

    Update_CategoryID and category_name

--- Mysql/sql_marts_insert.py
+++ Mysql/sql_marts_insert.py
@@ -1,9 +1,8 @@
-from sql_model import Mart, Product, Category, Price, BASE
+from sql_model import Mart, Product, Category, Price, BASE, engine
 import sqlalchemy as sa
 
 
 # 使用sa.create_engine設定數據庫的連接信息
-engine = sa.create_engine('mysql+pymysql://root:00065638@localhost:3306/demo1', echo=True)
 # 定義session 可以去連接數據庫 用來插入數據, 查詢數據
 Session = sa.orm.sessionmaker(bind=engine)
 # 通過調用Base裡面的metadata.create_all()方法把engine傳入去建立數據庫
@@ -16,11 +15,11 @@ rt_mart = Mart(id='R', mart_name='大潤發')
 carrefour_mart = Mart(id='C', mart_name='家樂福')
 poya_mart = Mart(id='Y', mart_name='寶雅')
 
-<<<<<<< HEAD
-# all_marts = [px_marts, rt_mart, carrefour_mart, poya_mart]
 
-# all_marts = [px_marts, rt_mart, carrefour_mart, poya_mart]
-=======
+all_marts = [px_marts, rt_mart, carrefour_mart, poya_mart]
+
+
+
 """生鮮食品：fresh_food
 冷凍食品：frozen_food
 飲料零食：drink_snacks
@@ -34,18 +33,18 @@ poya_mart = Mart(id='Y', mart_name='寶雅')
 大小家電：electrical_3C
 """
 # add Category
-fresh_food = Category(id=1, category_name='fresh_food')
-frozen_food = Category(id=2, category_name='frozen_food')
-drink_snacks = Category(id=3, category_name='drink_snacks')
-rice_oil_powder = Category(id=4, category_name='rice_oil_powder')
-make_up = Category(id=5, category_name='make_up')
-baby = Category(id=6, category_name='baby')
-life_style = Category(id=7, category_name='life_style')
-daily_use = Category(id=8, category_name='daily_use')
-furniture = Category(id=9, category_name='furniture')
-clothing = Category(id=10, category_name='clothing')
-electrical_3C = Category(id=11, category_name='electrical_3C')
->>>>>>> feature/pxmart-scraper
+fresh_food = Category(id="fresh_food", category_name='生鮮食品')
+frozen_food = Category(id="frozen_food", category_name='冷凍食品')
+drink_snacks = Category(id="drink_snacks", category_name='飲料零食')
+rice_oil_powder = Category(id="rice_oil_powder", category_name='米油沖泡')
+make_up = Category(id="make_up", category_name='美妝護理')
+baby = Category(id="baby", category_name='母嬰保健')
+life_style = Category(id="life_style", category_name='生活休閒')
+daily_use = Category(id="daily_use", category_name='日用百貨')
+furniture = Category(id="furniture", category_name='家具寢飾')
+clothing = Category(id="clothing", category_name='服飾鞋包')
+electrical_3C = Category(id="electrical_3C", category_name='大小家電')
+
 
 # 建立一個session
 session = Session()
