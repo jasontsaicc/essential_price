48d94505b58718c45fac95daf0d7e312b4bb8103
jasontsaicc <jason.tsaicc@gmail.com>
2022-09-08 09:02:09 +0800

    create Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,46 +1,59 @@
-import os
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask
 from flask import send_from_directory, render_template
-import numpy as np
-import tempfile
-import cv2
-from md_test import milk_model
-from perdiction import image, predict
-
-# 檔案上傳的路徑
-UPLOAD_FOLDER = './test_img/'
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+from flask import request
 
 app = Flask(__name__)
+db = SQLAlchemy()
+
+# MySql datebase
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://admin:tgi102aaa@projectdb.ckq7h3eivlb4.ap-northeast-1.rds.amazonaws.com:3306/essential"
+
+db.init_app(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file'].read()
-        file = np.fromstring(file, np.uint8)
-        file = cv2.imdecode(file, cv2.IMREAD_COLOR)[:, :, ::-1]
-        milk_model(file)
-        result = milk_model(file)
+
+# Read data
 
 
-        return redirect(url_for('uploaded_file', filename=result))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+@app.route('/')
+def hello_world():  # put application's code here
 
-#
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return render_template('result.html', filename=filename)
+
+    return render_template('index-2.html')
+
+
+@app.route('/index')
+def index():  # put application's code here
+    return render_template('index-2.html')
+
+
+@app.route('/shop')
+def shop():  # put application's code here
+    return render_template('shop.html')
+
+
+
+@app.route('/shop-details/<Product_id>')
+def shop_details(Product_id):  # put application's code here
+
+    prduct_sql = f"""SELECT product.id, product_name, product_pic_url, price from product JOIN price where product.id = price.product_id and product.id = {Product_id}"""
+
+    query_data = db.engine.execute(prduct_sql)
+    query_data_list = []
+
+    for i in query_data:
+        query_data_list.append(i)
+
+    print(query_data_list)
+    Product_id = query_data_list[0][0]
+    name = query_data_list[0][1]
+    pic_url = query_data_list[0][2]
+    price = query_data_list[0][3]
+    return render_template('shop-details.html', Product_id=Product_id, name=name, pic_url=pic_url, price=price)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
