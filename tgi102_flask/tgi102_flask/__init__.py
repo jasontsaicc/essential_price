ae08e573a35ba78c74f567a6d9f929040b932cde
jasontsaicc <jason.tsaicc@gmail.com>
2022-09-19 15:37:58 +0800

    update_requirements.txt

--- tgi102_flask/tgi102_flask/__init__.py
+++ tgi102_flask/tgi102_flask/__init__.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from flask import request, render_template, redirect, url_for
 from flask_paginate import Pagination, get_page_parameter
-from flask_msearch import Search
 
 db = SQLAlchemy()
 
@@ -19,9 +18,9 @@ def create_app():
     app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
     db.init_app(app)
-    search = Search()
-    search.init_app(app)
-    search.create_index()
+    # # search = Search()
+    # search.init_app(app)
+    # search.create_index()
     app.app_context().push()
 
     return app
