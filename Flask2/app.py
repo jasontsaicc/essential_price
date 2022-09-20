1977d675beae64c3b2053394aaa3524756df58c3
FrankWuWei <110382778+FrankWuWei@users.noreply.github.com>
2022-09-20 12:51:32 +0800

    Update app.py

--- Flask2/app.py
+++ Flask2/app.py
@@ -6,12 +6,10 @@ import numpy as np
 import tempfile
 import cv2
 from md_test import milk_model
-# from perdiction import image, predict
-
-# 檔案上傳的路徑
-# UPLOAD_FOLDER = './test_img/'
+import tensorflow as tf
 
 
+cv2.imdecode
 app = Flask(__name__)
 
 
@@ -20,9 +18,16 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file'].read()
         file = np.fromstring(file, np.uint8)
-        file = cv2.imdecode(file, cv2.IMREAD_COLOR)[:, :, ::-1]
-        milk_model(file)
-        result = milk_model(file)
+        file = cv2.imdecode(file, cv2.IMREAD_COLOR)
+
+        img = cv2.resize(file, (224, 224))
+        img = img.astype(np.float32)
+        img_array = []
+        img_array.append(img)
+        img_array = np.asarray(img_array)
+        
+        result = predict(img_array)
+        
 
 
         return redirect(url_for('uploaded_file', filename=result))
@@ -40,6 +45,32 @@ def upload_file():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return render_template('result.html', filename=filename)
+
+
+def predict(img_array):
+    
+    img = img_array
+    model_pred= tf.lite.Interpreter('./tflite_model.tflite')
+    model_pred.allocate_tensors()
+    input_index = model_pred.get_input_details()[0]["index"]
+    output_index = model_pred.get_output_details()[0]["index"]
+
+    model_pred.set_tensor(input_index, img)
+    model_pred.invoke()
+
+    output_data = model_pred.get_tensor(output_index)
+    print(output_data)
+    x = np.argmax(output_data)
+
+    brand = {0: '義美全脂鮮乳', 1: '林鳳營全脂鮮乳', 2: '乳香世家鮮乳', 3: '福樂一番鮮特極鮮乳', 4: '光泉鮮乳', 
+            5: '瑞穗全脂鮮奶', 6: '瑞穗全脂鮮奶', 7: '瑞穗低脂鮮奶', 8: '瑞穗低脂鮮奶'}
+
+    for i in brand:
+        if i == x:
+            pd_name = brand[i]
+            print("Prediction:", pd_name)
+    return pd_name
+
 
 
 if __name__ == '__main__':
