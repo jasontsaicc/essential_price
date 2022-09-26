f16d056ffef8070c75b62ffa08ab207137d69e24
jasontsaicc <jason.tsaicc@gmail.com>
2022-09-26 22:44:45 +0800

    update dockerfile

--- tgi102_flask/tgi102_flask/model.py
+++ tgi102_flask/tgi102_flask/model.py
@@ -57,46 +57,51 @@ class Product(BASE):
 
 
 def get_index_data():
-    # index_sql = db.session.query(Product, Price).filter(Product.id == Price.product_id).order_by(db.func.rand()).limit(8)
-    index_sql = db.session.query(Product, Price, Mart).filter(Product.id == Price.product_id).order_by(db.func.rand()).limit(8)
-
     index_sql_raw = """SELECT DISTINCT *  FROM (SELECT product.id, product.product_name, product.product_url, product.product_pic_url, product.mart_id, price.date, price.price FROM product JOIN price ON (product.id=price.product_id)) AS A join marts on (A.mart_id = marts.id) ORDER BY RAND() LIMIT 8"""
     index_cursor = db.session.execute(index_sql_raw)
     index_results = index_cursor.fetchall()
 
+    # sqlalchemy
+    # index_sql = db.session.query(Product, Price).filter(Product.id == Price.product_id).order_by(db.func.rand()).limit(8)
     index_data_list = []
-    for i in index_sql:
-        index_data_list.append(i)
-    print("*"*100)
-    print("index_data_list", index_data_list)
-    print("index_results", index_results)
-    print("index_results[0][0]", index_results[0][0])
-    print("index_results[0][1]", index_results[0][1])
-    print("index_results[0][2]", index_results[0][2])
-    print("index_results[0][3]", index_results[0][3])
-    print("index_results[0][5]", index_results[0][5])
-    print("index_results[0][6]", index_results[0][6])
-    print("index_results[0][7]", index_results[0][7])
-    print("index_results[0][8]", index_results[0][8])
+    # for i in index_sql:
+    #     index_data_list.append(i)
 
+    print("*"*100)
+    print("index_results", index_results)
     return index_results
 
 
 def get_count_category(category_id):
     count_category = db.session.query(db.func.count(Product.id)).filter(Product.id == Price.product_id).filter(
         Product.category_id == category_id).all()[0][0]
+
     return count_category
 
 
 def get_category_product(category_id, end):
-    category_product = db.session.query(Product, Price).filter(Product.id == Price.product_id).filter(
-        Product.category_id == category_id).order_by(db.func.rand()).limit(12).offset(end)
+    # category_product = db.session.query(Product, Price).filter(Product.id == Price.product_id).filter(
+    #     Product.category_id == category_id).order_by(db.func.rand()).limit(12).offset(end)
 
-    query_data_list = []
+    category_product_sql = f"SELECT DISTINCT pd.id,pd.product_name,pd.product_url,pd.product_pic_url,pr.DATE,pr.price,m.mart_name,pd.category_id FROM product AS pd JOIN price AS pr ON pd.id=pr.product_id JOIN marts AS m ON pd.mart_id=m.id WHERE pd.category_id='{category_id}' ORDER BY RAND() LIMIT 12 OFFSET {end}"
+    category_product = db.session.execute(category_product_sql)
+    category_product_results = category_product.fetchall()
+    # query_data_list = []
+    #
+    # for i in category_product:
+    #     query_data_list.append(i)
+    print("*"*100)
+    print("category_product_results", category_product_results)
+    print("category_product_results[0][0]", category_product_results[0][0])
+    print("category_product_results[0][1]", category_product_results[0][1])
+    print("category_product_results[0][2]", category_product_results[0][2])
+    print("category_product_results[0][3]", category_product_results[0][3])
+    print("category_product_results[0][4]", category_product_results[0][4])
+    print("category_product_results[0][5]", category_product_results[0][5])
+    print("category_product_results[0][6]", category_product_results[0][6])
+    print("category_product_results[0][7]", category_product_results[0][7])
 
-    for i in category_product:
-        query_data_list.append(i)
-    return query_data_list
+    return category_product_results
 
 
 def get_shop_details(product_id_query):
@@ -106,5 +111,6 @@ def get_shop_details(product_id_query):
     query_data_list = []
     for i in shop_details:
         query_data_list.append(i)
+    # print()
 
     return query_data_list
